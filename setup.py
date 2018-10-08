e88188416b6b3afd549ded2ea9f2094a050fbf86
Jan Clemens <clemensjan@googlemail.com>
2018-10-08 10:49:17 +0200

    fixed requirements
    need to test whether opencv -python comes with ffmpeg bindings on all systems

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     version='0.1',
     author='Jan Clemens',
     py_modules=['videoreader'],
+    python_requires='>=3.5',
     install_requires=[
         'opencv-python>=3.0',
-        #'python>=3.5',
-        'numpy',
+        'numpy>=1.14',
     ],
     keywords='cv2 opencv videocapture videoreader avi',
 )
