bfc1fbf8b0afff8da5abbeb764358e1a9d29762b
Jan Clemens <clemensjan@googlemail.com>
2018-11-17 21:10:48 +0100

    remove unnecessary numpy dependency and bump minor version

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from setuptools import setup
 
 setup(
     name='videoreader',
-    version='0.1',
+    version='0.2',
     author='Jan Clemens',
     py_modules=['videoreader'],
     python_requires='>=3.5',
     install_requires=[
         'opencv-python>=3.0',
-        'numpy>=1.14',
     ],
     keywords='cv2 opencv videocapture videoreader avi',
 )
