db74aa232a3c3d5f4403b0f6c4bfec11b29d85a0
Jan Clemens <clemensjan@googlemail.com>
2019-11-29 09:17:03 +0100

    fixed version info in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
 from setuptools import setup
+import os
+import codecs
+import re
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 
 setup(
     name='videoreader',
-    version=version,
+    version=find_version("package", "__init__.py"),
     author='Jan Clemens',
     py_modules=['videoreader'],
     python_requires='>=3.5',
